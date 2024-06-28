be66749007a2f0a92529b98e05c1ba38fa6ef6ed
V-I-N-I-C-I-U-S <sd.vinicius.main@gmail.com>
2024-06-27 22:00:20 -0300

    tentativa correção pages.tsx

--- Trainee2024/landing-page/src/app/page.tsx
+++ Trainee2024/landing-page/src/app/page.tsx
@@ -1,12 +1,44 @@
-"use client";
-import styles from "~/styles/homepage.module.css";
-import Image from "next/image"
+import Link from "next/link";
+import styles from "~/styles/index.module.css";
+import { OlaMundo } from "../_components/olaMundo";
 import { Quadradinhos } from "./backup/quadradinhos";
 
 export default function Home() {
   return (
-    <main>
-     <Quadradinhos/>
+    <main className={styles.main}>
+      <OlaMundo/>
+      <Quadradinhos/>
     </main>
   );
 }
+
+
+{/* <div className={styles.container}>
+        <h1 className={styles.title}>
+          Create <span className={styles.pinkSpan}>T3</span> App
+        </h1>
+        <div className={styles.cardRow}>
+          <Link
+            className={styles.card}
+            href="https://create.t3.gg/en/usage/first-steps"
+            target="_blank"
+          >
+            <h3 className={styles.cardTitle}>First Steps →</h3>
+            <div className={styles.cardText}>
+              Just the basics - Everything you need to know to set up your
+              database and authentication.
+            </div>
+          </Link>
+          <Link
+            className={styles.card}
+            href="https://create.t3.gg/en/introduction"
+            target="_blank"
+          >
+            <h3 className={styles.cardTitle}>Documentation →</h3>
+            <div className={styles.cardText}>
+              Learn more about Create T3 App, the libraries it uses, and how to
+              deploy it.
+            </div>
+          </Link>
+        </div>
+      </div> */}
