8ee75b2771a44ab82c9e9a693aafe92c9fec50d7
leotsampaio <leotsampaio@gmail.com>
2024-07-27 21:00:57 -0300

    advance

--- Trainee2024/estoque/src/app/api/transacao/route.ts
+++ Trainee2024/estoque/src/app/api/transacao/route.ts
@@ -3,23 +3,18 @@ import { Produto, User } from "prisma";
 import { db } from "~/server/db";
 export async function GET(req: NextRequest) {
     try {
-        const id = req.nextUrl.searchParams.get('id')
-        const time = await db.transacao.findMany({
-            orderBy: {
-                data:"asc"
-            })
-
-        if (id) {
-            const transacao = await db.transacao.findUnique({
-                where: {
-                    id: Number(id)
-                }
-            })
-            return NextResponse.json({ message: "OK", transacao })
-        } else {
-            const transacoes = await db.transacao.findMany()
-            return NextResponse.json({ message: "OK", transacoes })
-        }
+      const response = (await req.json()) as ( daysPast: number );
+      const { daysPast } = response;
+      const time = await db.transacao.findMany({
+           orderBy: {
+              data: "asc",
+            },
+      });
+      const today = new Date();
+      const timeFiltered = time.filter((transacao) =>{
+          today.getTime() - transacao.data.getTime() > daysPast * 86400000;
+      })
+      return NextResponse.json({data : timeFiltered})
     } catch (err) {
         if (err instanceof Error) {
             return NextResponse.json(
@@ -35,7 +30,7 @@ export async function GET(req: NextRequest) {
     }
 }
 export async function POST(req: NextRequest) {
-    const { tipo ,quantidade, userId ,produtoId} = await req.json() as { tipo: string , quantidade: Number , userId: string , produtoId: Number}
+    const { tipo ,quantidade, userId ,produtoId} = await req.json() as { tipo: string , quantidade: number , userId: string , produtoId: number}
     try {
         const transacao = await db.transacao.create({
             data: {
@@ -63,7 +58,7 @@ export async function POST(req: NextRequest) {
 }
 
 export async function PATCH(req: NextRequest) {
-    const { id, tipo, quantidade } = await req.json() as { tipo: string, quantidade: Number, id: Number }
+    const { id, tipo, quantidade } = await req.json() as { tipo: string, quantidade: number, id: number }
     try {
         const transacao = await db.transacao.update({
             where: {
@@ -91,9 +86,9 @@ export async function PATCH(req: NextRequest) {
 }
 
 export async function DELETE(req: NextRequest) {
-    const { id } = await req.json() as { id: Number }
+    const { id } = await req.json() as { id: number }
     try {
-        const transacao = await db.Transacao.delete({
+        const transacao = await db.transacao.delete({
             where: {
                 id
             },
